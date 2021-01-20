c7966ec3be8f252325522bd1032f9dd0135f0606
Willpower-Bazler <54726673+Willpower-Bazler@users.noreply.github.com>
2021-01-19 19:33:13 -0800

    Add files via upload

---
+++ assignment1/sketch.js
@@ -0,0 +1,72 @@
+var c;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(0);
+  
+  fill(128);
+  stroke(0)
+  
+  square(25,25,550)
+  
+  c = random(192,255)
+  
+  fill(c);
+  stroke(0)
+  
+  circle(380,300,220)
+  circle(390,300,210)
+  circle(400,300,200)
+  circle(410,300,190)
+  circle(420,300,180)
+  circle(430,300,170)
+  circle(440,300,160)
+  circle(450,300,150)
+  circle(460,300,140)
+  circle(470,300,130)
+  circle(480,300,120)
+  circle(490,300,110)
+  circle(500,300,100)
+  circle(510,300,90)
+   circle(520,300,80)
+   circle(530,300,70)
+  circle(540,300,60)
+  circle(550,300,50)
+  
+  rect(180,400,70,10)
+  rect(180,425,80,10)
+  rect(180,450,90,10)
+  rect(180,475,100,10)
+  rect(180,500,110,10)
+  
+  rect(175,175,50,350)
+  
+
+  square(300,300,275)
+  
+  square(100,100,100)
+  square(175,175,200)
+ 
+  triangle(500,500,100,200,200,100)
+  
+  fill(0)
+ textSize(16);
+ text('will bazler', 475, 550);
+  
+  textSize(8);
+ text('01101101 01101111', 35,45)
+  text('01101110 01101111', 35,55)
+  text('01100011 01101000', 35,65)
+  text('01110010 01101111', 35,75)
+  text('01101101 01100101', 35,85)
+  
+      
+      
+      
+      
+  
+  
+}
